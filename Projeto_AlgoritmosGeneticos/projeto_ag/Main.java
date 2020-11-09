ba4f622ed162986f763199515de4196769fbb4ce
Thomas Adam da Costa <thomas.costa@alunos.ifsuldeminas.edu.br>
2020-11-08 22:22:46 -0300

    Update Main.java

--- Projeto_AlgoritmosGeneticos/projeto_ag/Main.java
+++ Projeto_AlgoritmosGeneticos/projeto_ag/Main.java
@@ -19,19 +19,19 @@ public class Main {
         int corte;  // posicao de corte do crossover padrao ([2])
 
         Grafo grafo; // instancia do grafo
-        AlgoritimoGenetico a;
+        AlgoritimoGenetico a; // instancia do algoritmo
         
         System.out.println("Projeto Algorítmos Genéticos");
         System.out.println("");
         System.out.println("Número de Gerações definido: "+ geracoes); // numero de gerações impresso
-        int op = 2; // opcao de grafo
-        g = new Grafo(op);
-        g.definepontas();
+        int op = 2; // opcao de grafo, definido estaticamente
+        grafo = new Grafo(op);
+        grafo.definepontas();
         System.out.println("Tamanho de População: "+ populacao);
         System.out.println("Membros de Elite: "+ elitismo);
         System.out.println("Taxa de Mutação: "+ mutacao);
-        System.out.println("Método de Seleção: "+ g.printSelecao(selecao));
-        corte = g.posicaoCorte();
+        System.out.println("Método de Seleção: "+ grafo.printSelecao(selecao));
+        corte = grafo.posicaoCorte();
         a = new AlgoritimoGenetico(populacao, geracoes, mutacao, elitismo, selecao, combatentes, grafo, corte);
     }
 }
